f5c1b743bbce404ca52ac2ead6d3dedbc3c4a3be
Fluncos <lolyyyt199@gmail.com>
2026-02-12 14:10:17 -0300

    Refactor Premiums key generation using id function

--- Cliente-data.lua
+++ Cliente-data.lua
@@ -1,12 +1,12 @@
-local function n(a,b) 
-    return a + b 
+local function id(a,b)
+    return tonumber(a .. b)
 end
 
 return {
     Premiums = {
-        [n(3640000000, 452457)] = true,
-        [n(10432000000, 456952)] = true,
-        [n(9691000000, 113640)] = true,
-        [n(6152000000, 611291)] = true
+        [id("3640452","457")] = true,
+        [id("10432456","952")] = true,
+        [id("9691113","640")] = true,
+        [id("6152611","291")] = true
     }
 }
