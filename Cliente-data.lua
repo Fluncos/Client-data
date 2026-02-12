3efa21c249bf46de757e026ab5ce381c12c052d9
Fluncos <lolyyyt199@gmail.com>
2026-02-12 14:05:51 -0300

    Refactor Premiums calculation using a new function

--- Cliente-data.lua
+++ Cliente-data.lua
@@ -1,18 +1,12 @@
-local function _(a,b,c,d)
-    return (a*10^7)+(b*10^3)+c+d
+local function n(a,b) 
+    return a + b 
 end
 
-local t={
-    {364,4524,0,457},
-    {1043,2456,0,952},
-    {969,1113,0,640},
-    {615,2611,0,291}
+return {
+    Premiums = {
+        [n(3640000000, 452457)] = true,
+        [n(10432000000, 456952)] = true,
+        [n(9691000000, 113640)] = true,
+        [n(6152000000, 611291)] = true
+    }
 }
-
-local p={}
-for i=1,#t do
-    local v=t[i]
-    p[_(v[1],v[2],v[3],v[4])] = true
-end
-
-return{Premiums=p}
