9609ca55787d6e23975844dabe004a3f1ca927cd
Fluncos <lolyyyt199@gmail.com>
2026-03-16 15:32:25 -0300

    Refactor Cliente-data.lua to remove id function
    Removed the id function and its usage in the Premiums table.

--- Cliente-data.lua
+++ Cliente-data.lua
@@ -1,12 +1,5 @@
-local function id(a,b)
-    return tonumber(a .. b)
-end
-
 return {
     Premiums = {
-        [id("3640452","457")] = true,
-        [id("10432456","952")] = true,
-        [id("9691113","640")] = true,
-        [id("6152611","291")] = true
+
     }
 }
